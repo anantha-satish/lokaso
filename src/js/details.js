066b748d73b5732b7b52ee7ce6f756c21de87b06
satish-ui-developer <ananthanike@gmail.com>
2023-06-28 01:54:29 +0530

    cart completed

--- src/js/details.js
+++ src/js/details.js
@@ -1,27 +1,51 @@
-const triggerdetails = document.querySelectorAll(".trigger-details");
-const enterdetails = document.getElementById("enter-details");
-const addinlinecart = document.getElementById("add-inline-cart");
+const bookdetails = document.getElementById("book-details");
+const giftdetails = document.getElementById("gift-details");
+const triggerbookexperience = document.getElementById("trigger-book-experience");
+const triggergiftexperience = document.getElementById("trigger-gift-experience");
+const defaultcart = document.getElementById("default-cart");
 const experiencebtns = document.getElementById("experience-btns");
+
+const defaultcheckout = document.getElementById("default-checkout");
+const giftcheckout = document.getElementById("gift-checkout");
 
 function _toggleInlineCartView() {
     experiencebtns.classList.toggle('d-flex');
     experiencebtns.classList.toggle('d-none');
-    enterdetails.classList.toggle('d-block');
-    enterdetails.classList.toggle('d-none');
 }
-
-if (triggerdetails) {
-    for (let index = 0; index < triggerdetails.length; index++) {
-        const element = triggerdetails[index];
-        element.addEventListener("click", function () {
-            _toggleInlineCartView();
-        });
-    }
-}
-
-if (addinlinecart) {
-    addinlinecart.addEventListener("click", function () {
+if (triggerbookexperience) {
+    triggerbookexperience.addEventListener("click", function () {
         _toggleInlineCartView();
+        bookdetails.classList.toggle('d-block');
+        bookdetails.classList.toggle('d-none');
+    });
+}
+
+if (triggergiftexperience) {
+    triggergiftexperience.addEventListener("click", function () {
+        _toggleInlineCartView();
+        giftdetails.classList.toggle('d-block');
+        giftdetails.classList.toggle('d-none');
+    });
+}
+
+if (defaultcart) {
+    defaultcart.addEventListener("click", function () {
+        _toggleInlineCartView();
+        bookdetails.classList.toggle('d-block');
+        bookdetails.classList.toggle('d-none');
+    });
+}
+
+if (defaultcheckout) {
+    defaultcheckout.addEventListener("click", function () {
+        window.location.href = "./checkout.html";
+    });
+}
+
+
+if (giftcheckout) {
+    giftcheckout.addEventListener("click", function () {
+        window.location.href = "./gift-checkout.html";
     });
 }
 
