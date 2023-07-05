718c835fb62a4888c7106b6d8cad37d1013fd325
satish-ui-developer <ananthanike@gmail.com>
2023-07-06 01:30:34 +0530

    new ui changes are added

---
+++ dist/cart.4f7198ba.js
@@ -0,0 +1,1 @@
+(()=>{const e=document.getElementById("cancel-carting");e&&e.addEventListener("click",(()=>{window.history.back()}));const t=document.querySelectorAll(".btn-go-details");if(t)for(let e=0;e<t.length;e++)t[e].addEventListener("click",(()=>{window.location.href="./details.html"}))})();
